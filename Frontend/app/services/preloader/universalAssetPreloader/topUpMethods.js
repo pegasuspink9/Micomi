40dc56881128e051ddb8d0f3d396f69b562b9da7
Leon Dev <pegasuspink9@gmail.com>
2026-05-22 11:29:54 +0800

    Update topUpMethods.js

--- Frontend/app/services/preloader/universalAssetPreloader/topUpMethods.js
+++ Frontend/app/services/preloader/universalAssetPreloader/topUpMethods.js
@@ -1,17 +1,3 @@
-export const TOP_UP_IMAGE_MAP = {
-  // Coins
-  'currency_coins_1000': 'https://micomi-assets.me/Top%20ups/coins/handful.png',
-  'currency_coins_4000': 'https://micomi-assets.me/Top%20ups/coins/pounce.png',
-  'currency_coins_10000': 'https://micomi-assets.me/Top%20ups/coins/pounce.png',
-  // Diamonds
-  'currency_diamonds_50': 'https://micomi-assets.me/Top%20ups/diamonds/handful.png',
-  'currency_diamonds_160': 'https://micomi-assets.me/Top%20ups/diamonds/pounces.png',
-  'currency_diamonds_360': 'https://micomi-assets.me/Top%20ups/diamonds/chest.png',
-  // Energy
-  'infinite_energy_monthly': 'https://micomi-assets.me/Top%20ups/energy/Monthly.png',
-  'infinite_energy_lifetime': 'https://micomi-assets.me/Top%20ups/energy/Lifetime.png',
-};
-
 export const TOP_UP_COVER_URL = 'https://micomi-assets.me/Top%20ups/Cover/Cover.jpg';
 export const TOP_UP_BOARD_URL = 'https://micomi-assets.me/Top%20ups/Cover/Board.png';
 export const TOP_UP_SHOP_BG_URL = 'https://micomi-assets.me/Top%20ups/Cover/ShopBackground.png';
@@ -27,10 +13,6 @@ export const topUpMethods = {
         assets.push({ url, name, type, category });
       }
     };
-
-    Object.entries(TOP_UP_IMAGE_MAP).forEach(([itemId, url]) => {
-      addAsset(url, `topup_${itemId}`, 'image', 'top_up_shop');
-    });
 
     // Cover, Board & Shop Background
     addAsset(TOP_UP_COVER_URL, 'topup_cover', 'image', 'top_up_shop');
