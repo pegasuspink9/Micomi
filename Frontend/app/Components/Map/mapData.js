19db01eb9aa430859094d390540f868d0e715e30
Leon Dev <pegasuspink9@gmail.com>
2026-05-25 22:35:36 +0800

    Update mapData.js

--- Frontend/app/Components/Map/mapData.js
+++ Frontend/app/Components/Map/mapData.js
@@ -17,7 +17,7 @@ export const map = [{
 }, {
     map_id: 'map4',
     map_name: 'Computer',
-    map_image: { uri: 'https://micomi-assets.me/Lottie/Autumn/Islands.lottie' },
+    map_image: { uri: 'https://lottie.host/embed/6d4f857f-1645-4749-bf91-9ae83cd6a059/iVPUPbbr6a.lottie' },
     is_active: true,
 }];
 
