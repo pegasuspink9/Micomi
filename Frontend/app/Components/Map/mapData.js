3e9ddaec25d8ce8d48db83306a512c4210162a01
Leon Dev <pegasuspink9@gmail.com>
2026-05-25 17:31:30 +0800

    Update mapData.js

--- Frontend/app/Components/Map/mapData.js
+++ Frontend/app/Components/Map/mapData.js
@@ -2,17 +2,17 @@
 export const map = [{
     map_id: 'map1',
     map_name: 'HTML',
-    map_image: { uri: 'https://lottie.host/9875685d-8bb8-4749-ac63-c56953f45726/UnBHY7vAPX.json' },
+    map_image: { uri: 'https://micomi-assets.me/Lottie/HTML/Greenland.lottie' },
     is_active: true,
 }, {
     map_id: 'map2',
     map_name: 'CSS',
-    map_image: { uri: 'https://lottie.host/ce3ef58a-1a99-4698-a754-019c318f0656/SVy2FKaq7k.lottie' },
+    map_image: { uri: 'https://micomi-assets.me/Lottie/CSS/SVy2FKaq7k.lottie' },
     is_active: true,
 }, {
     map_id: 'map3',
     map_name: 'JavaScript',
-    map_image: { uri: 'https://lottie.host/aefb958d-9205-4ced-aba5-204adf453a99/Vm1zPhoNYp.lottie' },
+    map_image: { uri: 'https://micomi-assets.me/Lottie/JavaScript/Vm1zPhoNYp.lottie' },
     is_active: true,
 }, {
     map_id: 'map4',
